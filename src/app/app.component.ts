1acf677d70c6d5727e5149ccc6aeeff62d8b0ec2
Dekpo Yologaza <dekpo@me.com>
2021-03-12 10:03:07 +0100

    merge a sidemenu template to a tabs template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public appPages = [
+    { title: 'Tab 1', url: '/tabs/tab1', icon: 'triangle' },
+    { title: 'Tab 2', url: '/tabs/tab2', icon: 'ellipse' },
+    { title: 'Tab 3', url: '/tabs/tab3', icon: 'square' },
+    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
